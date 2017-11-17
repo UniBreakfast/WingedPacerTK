5f25e85723d4a1e8f69ba64bd0f1e63ab6c81227
UniBreakfast <unibreakfast1@gmail.com>
2017-11-17 21:58:35 +0200

    30

--- WingedPacerTK/tkpacer.py
+++ WingedPacerTK/tkpacer.py
@@ -28,8 +28,9 @@ def decision_window():
 
     bt_decide.config(relief='sunken')
     w_decision = tki.Toplevel(pacer, name='w_decision')
-    w_decision.geometry('380x250+460+140')
+    w_decision.geometry('380x180+460+140')
     w_decision.transient(pacer)
+    w_decision.title('итак...')
     w_decision.focus()
 
     def w_decision_rip():
@@ -39,13 +40,48 @@ def decision_window():
 
     Label30('Что ты решил сделать?', w_decision).pack()
 
-    frm_input = tki.Frame(w_decision)
+    frm_input = tki.Frame(w_decision, padx=6)
     frm_input.pack()
 
-    Label('Я обязуюсь', frm_input).pack(side='left', padx=3, pady=3, anchor='n')
+    subfrm_input = tki.Frame(frm_input)
+    subfrm_input.pack(side='left')
 
-    ntr_input = st.ScrolledText(frm_input, font=treb14)
-    ntr_input.pack(side='right', padx=2, pady=4)
+    Label('Я обязуюсь', subfrm_input).pack(side='top', padx=3, anchor='e')
+
+
+    ntr_input = st.ScrolledText(frm_input, font=treb14, padx=5, pady=2, height=4,
+                                wrap='word')
+    ntr_input.insert('1.0', decision)
+    ntr_input.pack(side='right')
+
+    def set_goal(_=None):
+        global decision
+        new_decision = ntr_input.get('1.0', 'end-1c').strip().strip('.')
+        if new_decision and new_decision != decision and 'w_question' in pacer.children:
+            pacer.children['w_question'].children['!label'].config(text='Ты обещал\n'+
+                                                            new_decision+ '.\nСправился?')
+        decision = new_decision
+        if decision:    bt_question.config(state='normal', borderwidth=3)
+        else:
+            bt_question.config(state='disabled', borderwidth=1)
+            if 'w_question' in pacer.children:
+                pacer.children['w_question'].destroy()
+                bt_question.config(relief='raised')
+        with open('userdata.txt', 'w', encoding='utf-8') as file:
+            file.write(decision)
+        return 'break'
+
+    ntr_input.bind('<Control-Return>', set_goal)
+    Button('btn_set', 'Решено!', set_goal, subfrm_input).pack(side='bottom',
+           padx=8, pady=11, anchor='s')
+
+try:
+    with open('userdata.txt', encoding='utf-8') as file:
+        decision = file.read()
+except:
+    decision = ''
+
+
 bt_decide = Button('bt_decide', 'Решить и Взяться', decision_window)
 bt_decide.pack()
 
@@ -56,7 +92,7 @@ def question_window():
 
     bt_question.config(relief='sunken')
     w_question = tki.Toplevel(pacer, name='w_question')
-    w_question.geometry('242x180+460+360')
+    w_question.title('кажется...')
     w_question.transient(pacer)
     w_question.focus()
 
@@ -65,7 +101,12 @@ def question_window():
         bt_question.config(relief='raised')
     w_question.protocol('WM_DELETE_WINDOW', w_question_rip)
 
-    Label30('Ты сделал то, что собирался?', w_question).pack()
+    question = Label30('Ты обещал\n'+decision+'.\nСправился?', w_question)
+    question.pack()
+    question.update()
+    question.config(wraplength='215')
+    w_question.geometry('250x'+str(110+question.winfo_height())+'+460+360')
+
 
     frm_buttons = tki.Frame(w_question)
     frm_buttons.pack()
@@ -95,8 +136,10 @@ def question_window():
     bt_no.pack(side='right', padx=10)
 
     lb_response = Label30('Признавайся, не таясь!', w_question)
+    lb_response.config()
     lb_response.pack()
 bt_question = Button('bt_question', 'Отчитаться', question_window)
+if not decision:    bt_question.config(state='disabled', borderwidth=1)
 bt_question.pack(pady=10)
 
 
